d724f7c4b2ffa70cb7b6b76fae8791569d8adb30
CHEUNG <dcheungaa@ust.hk>
2019-10-22 02:09:11 +0800

    done eightpoint

--- assgn3/matlab/eightpoint.m
+++ assgn3/matlab/eightpoint.m
@@ -7,3 +7,43 @@ function F = eightpoint(pts1, pts2, M)
 % Q2.1 - Todo:
 %     Implement the eightpoint algorithm
 %     Generate a matrix F from correspondence '../data/some_corresp.mat'
+
+%% Normalize pts
+
+n1 = pts1 / M;
+n2 = pts2 / M;
+
+%% Calculate Rank 3 F
+
+u = n1(:, 1);
+v = n1(:, 2);
+x = n2(:, 1);
+y = n2(:, 2);
+
+A = [u.*x u.*y u v.*x v.*y v x y ones(size(u))];
+
+[~, ~, V] = svd(A);
+
+F3 = reshape(V(:, end), 3, 3)';
+
+%% Calculate Rank 2 F
+
+[O, P, Q] = svd(F3);
+
+P(end, end) = 0;
+
+F2 = O * P * Q';
+
+%% Refine solution
+
+FR = refineF(F2, n1, n2);
+
+%% Un-normalize F
+
+S = [
+    1/M 0 0;
+    0 1/M 0;
+    0 0 1;
+    ];
+
+F = S' * FR * S;
--- assgn3/matlab/testTempleCoords.m
+++ assgn3/matlab/testTempleCoords.m
@@ -3,5 +3,13 @@
 % Write your code here
 %
 
+load('../data/someCorresp.mat');
+I1 = imread('../data/im1.png');
+I2 = imread('../data/im2.png');
+
+F = eightpoint(pts1, pts2, M);
+
+displayEpipolarF(I1, I2, F);
+
 % save extrinsic parameters for dense reconstruction
-save('../data/extrinsics.mat', 'R1', 't1', 'R2', 't2');
+%save('../data/extrinsics.mat', 'R1', 't1', 'R2', 't2');
