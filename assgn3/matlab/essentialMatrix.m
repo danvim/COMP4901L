b6b60b1bc7326d7bac6ee9b7c63b7b85abb54c33
CHEUNG <dcheungaa@ust.hk>
2019-10-23 01:41:40 +0800

    essential

--- assgn3/matlab/essentialMatrix.m
+++ assgn3/matlab/essentialMatrix.m
@@ -8,3 +8,13 @@ function E = essentialMatrix(F, K1, K2)
 %   Returns:
 %       E:  Essential Matrix
 %
+
+% X_1' * E * X_2 = 0
+% (inv(K_1) * P_1)' * E * (inv(K_2) * P_2) = 0
+% P_1' * inv(K_1)' * E * inv(K_2) * P_2 = 0
+% P_1' * F * P_2 = 0
+% F = inv(K_1)' * E * inv(K_2)
+% K_1' * F = E * inv(K_2)
+% K_1' * F * K_2 = E
+
+E = K1' * F * K2;
--- assgn3/matlab/testTempleCoords.m
+++ assgn3/matlab/testTempleCoords.m
@@ -4,6 +4,7 @@
 %
 
 load('../data/someCorresp.mat');
+load('../data/intrinsics.mat');
 I1 = imread('../data/im1.png');
 I2 = imread('../data/im2.png');
 
@@ -11,7 +12,9 @@ F = eightpoint(pts1, pts2, M);
 
 % displayEpipolarF(I1, I2, F);
 
-epipolarMatchGUI(I1, I2, F);
+% epipolarMatchGUI(I1, I2, F);
+
+E = essentialMatrix(F, K1, K2);
 
 % save extrinsic parameters for dense reconstruction
 %save('../data/extrinsics.mat', 'R1', 't1', 'R2', 't2');
