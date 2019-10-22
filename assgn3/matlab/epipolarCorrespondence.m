ad799cf05641b82e524b8df2f421890b26fa6a98
CHEUNG <dcheungaa@ust.hk>
2019-10-23 01:02:41 +0800

    match points

--- assgn3/matlab/epipolarCorrespondence.m
+++ assgn3/matlab/epipolarCorrespondence.m
@@ -9,32 +9,44 @@ function [pts2] = epipolarCorrespondence(im1, im2, F, pts1)
 %       pts2:   coordinates of points in image 2
 %
 
-p1h = [pts1, ones([length(pts1), 1])];
+k = 7;
+
+p1h = [pts1, 1];
 l = (F * p1h')';
 
-[h, w] = size(im1);
+[h, w, ~] = size(im1);
 
 %% Search window along line
+padSize = (k-1)/2;
+I1 = padarray(im1, [padSize, padSize], 'replicate', 'both');
+I2 = padarray(im2, [padSize, padSize], 'replicate', 'both');
 
-G = [
-    1 2 1;
-    2 4 2;
-    1 2 1;
-    ];
-
-I1 = padarray(im1, [1, 1], 'replicate', 'both');
-I2 = padarray(im2, [1, 1], 'replicate', 'both');
-
-pts2 = [];
-
-for i = 1:length(pts1)
-    pos = pts1(1, :);
-    x = pos(1);
-    y = pos(2);
-    W = im1(x:x+3, y:y+3); % Source window
-    % Search through line: ax + by + c = 0 iterate y
-    ys = 1:h; % x = (-by - c)/a
-    xs = -(W(2) * ys - W(3)) / W(1);
-    ts = [xs ys]; % target
-    
+pos = pts1(1, :);
+x = floor(pos(1));
+y = floor(pos(2));
+W = double(I1(y:y+k-1, x:x+k-1, :)); % Source window
+% Search through line: ax + by + c = 0 iterate y
+% y = (-a/b)x - (c/b)
+m = -l(1)/l(2);
+if m > 1 || m < -1
+    ys = (1:h)'; % x = (-by - c)/a
+    xs = floor(-(l(2) * ys + l(3)) / l(1));
+else
+    xs = (1:w)';
+    ys = floor(-(l(1) * xs + l(3)) / l(2));
 end
+ts = [xs ys]; % target
+scores = zeros([length(ts), 1]);
+for j = 1:length(ts)
+    pos_ = ts(j, :);
+    x_ = pos_(1);
+    y_ = pos_(2);
+    if x_ < 1 || x_ > w || y_ < 1 || y_ > h
+        continue
+    end
+    V = double(I2(y_:y_+k-1, x_:x_+k-1, :));
+    scores(j) = sum(abs(W-V), 'all');
+end
+[~, bestI] = min(scores);
+pts2 = ts(bestI, :);
+
--- assgn3/matlab/testTempleCoords.m
+++ assgn3/matlab/testTempleCoords.m
@@ -9,7 +9,9 @@ I2 = imread('../data/im2.png');
 
 F = eightpoint(pts1, pts2, M);
 
-displayEpipolarF(I1, I2, F);
+% displayEpipolarF(I1, I2, F);
+
+epipolarMatchGUI(I1, I2, F);
 
 % save extrinsic parameters for dense reconstruction
 %save('../data/extrinsics.mat', 'R1', 't1', 'R2', 't2');
