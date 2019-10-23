eb19b8426fda29db533134b90538f69a1b57ac34
dipsywong98 <ycwongal@connect.ust.hk>
2019-10-23 11:04:21 +0800

    rectify hasnt tested

--- assgn3/matlab/rectify_pair.m
+++ assgn3/matlab/rectify_pair.m
@@ -3,3 +3,23 @@ function [M1, M2, K1n, K2n, R1n, R2n, t1n, t2n] = ...
 % RECTIFY_PAIR takes left and right camera paramters (K, R, T) and returns left
 %   and right rectification matrices (M1, M2) and updated camera parameters. You
 %   can test your function using the provided script q4rectify.m
+c1 = -inv(K1*R1)*(K1*t1);
+c2 = -inv(K2*R2)*(K2*t2);
+r1 = (c1-c2) / norm(c1 - c2);
+r2 = cross(R1(3,:), r1);
+r3 = cross(r2,r1);
+
+R = [r1 r2 r3]';
+K = K2;
+
+t1n = -R*c1;
+t2n = -R*c2;
+
+R1n = R;
+R2n = R2 * R;
+
+K1n = K1;
+K2n = K2;
+
+M1 = (K1n*R1n) * inv(K1 * R1);
+M2 = (K2n*R2n) * inv(K2 * R2);
