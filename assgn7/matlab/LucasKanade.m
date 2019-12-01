32207c9722e344f305973791e378f573250efe3e
CHEUNG <dcheungaa@ust.hk>
2019-12-02 00:50:16 +0800

    Think I've done 2.1

--- assgn7/matlab/LucasKanade.m
+++ assgn7/matlab/LucasKanade.m
@@ -1,1 +1,33 @@
-[u,v] = LucasKanade(It, It1, rect)
+function [u,v] = LucasKanade(It, It1, rect)
+
+th = 0.01;
+maxIter = 100;
+
+
+It1 = double(It1);
+x = rect(1);
+y = rect(2);
+w = rect(3);
+h = rect(4);
+
+it = double(It(y:y+h, x:x+w));
+
+s = fspecial('sobel');
+
+ix = imfilter(it, s', 'replicate');
+iy = imfilter(it, s, 'replicate');
+
+u = 0;
+v = 0;
+
+[Y, X] = meshgrid(y:y+h, x:x+w);
+
+for i = 1:maxIter
+    it1 = interp2(It1, X+u, Y+v)' - it;
+    vel = [ix(:), iy(:)] \ it1(:);
+    u = u + vel(1);
+    v = v + vel(2);
+    if max(abs(vel)) < th
+        break;
+    end
+end
---
+++ assgn7/matlab/test_lucas.m
@@ -0,0 +1,19 @@
+A = imread('../data/landing/frame0190_crop.jpg');
+B = imread('../data/landing/frame0194_crop.jpg');
+
+px = 482;
+py = 124;
+ww = 5;
+
+figure;
+imshow(A(py-ww:py+ww-1, px-ww:px+ww-1));
+title('Source Patch');
+
+[u, v] = LucasKanade(A, B, [px-ww, py-ww, ww*2, ww*2]);
+
+u_ = floor(u);
+v_ = floor(v);
+
+figure;
+imshow(B(py+v_-ww:py+v_+ww-1, px+u_-ww:px+u_+ww-1));
+title('Target Patch');
