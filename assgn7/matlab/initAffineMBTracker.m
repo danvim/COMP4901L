743db882567694b16aeef2f20e39e50649491e51
dipsywong98 <ycwongal@connect.ust.hk>
2019-12-02 19:19:17 +0800

    MB done T.T

--- assgn7/matlab/initAffineMBTracker.m
+++ assgn7/matlab/initAffineMBTracker.m
@@ -8,10 +8,14 @@ T = double(img(y:y+h-1, x:x+w-1))/255.0;
 [dTx,dTy] = gradient(T);
 
 % flatten to column vectors
+fprintf('size T');
+disp(size(T));
 T = T(:);
 dT = [dTx(:), dTy(:)];
 
-[X,Y] = meshgrid(y:y+h-1, x:x+w-1);
+[X,Y] = meshgrid(x:x+w-1, y:y+h-1);
+fprintf('size X');
+disp(size(X));
 X=X(:);
 Y=Y(:);
 
@@ -24,4 +28,5 @@ o=ones(size(X));
 J = [dT dT dT].*[X X Y Y o o];
 
 affineMBContext.J = J;
+affineMBContext.X = X;
 affineMBContext.invH = (J'*J)^(-1);
--- assgn7/matlab/mb_demo.m
+++ assgn7/matlab/mb_demo.m
@@ -1,18 +1,32 @@
 %tracker = [1 1 100 100]         % TODO Pick a bounding box in the format [x y w h]
-tracker = [435 83 130 55];         % TODO Pick a bounding box in the format [x y w h]
+% traker for landing
+tracker = [440    79   118    58];         % TODO Pick a bounding box in the format [x y w h]
 %tracker = [415 63 170 95];         % TODO Pick a bounding box in the format [x y w h]
 % You can use ginput to get pixel coordinates
 
 tracker = [212.0000  200.0000   20.0000   23.0000];
 
+
+do_car = 1;
+
 %% Initialize the tracker
 figure;
 
 % TODO run the Matthew-Baker alignment in both landing and car sequences
-%prev_frame = imread('../data/landing/frame0190_crop.jpg');
-prev_frame = imread('../data/car/frame0020.jpg');
+if do_car == 1
+    prev_frame = imread('../data/car/frame0020.jpg');
+    mov = VideoWriter('../mb_results/car.mp4');
+    r = 21:280;
+else
+    r = 191:308;
+    prev_frame = imread('../data/landing/frame0190_crop.jpg');
+    mov = VideoWriter('../mb_results/landing.mp4');
+end
+open(mov);
+%subplot(1,2,1)
 imshow(prev_frame);
-tracker = getrect;
+title('drag to create tracker')
+tracker = floor(getrect);
 x=tracker(1);
 y=tracker(2);
 w=tracker(3);
@@ -22,30 +36,43 @@ Win = Wp([0 0 0 0 0 0]);
 
 context = initAffineMBTracker(prev_frame, tracker);
 
+%pause
+
 %% Start tracking
 new_tracker = tracker
+
 %for i = 191:308
-for i = 21:280
+for i = r
     try
-%    im = imread(sprintf('../data/landing/frame%04d_crop.jpg', i));
-    im = imread(sprintf('../data/car/frame%04d.jpg', i));
+        if do_car
+            im = imread(sprintf('../data/car/frame%04d.jpg', i));
+        else
+            im = imread(sprintf('../data/landing/frame%04d_crop.jpg', i));
+        end
     catch E
-    continue
+        continue
     end
 %    template = prev_frame(y:y+h-1, x:x+w-1);
-    Wout = affineMBTracker(im, template, tracker, Win, context);
+    try
+        Wout = affineMBTracker(im, template, tracker, Win, context);
+    catch
+        continue
+    end
 
 %    new_tracker = ?? % TODO calculate the new bounding rectangle
-    xy1 = Wout*[x;y;1];
-%    xy2 = Wout*[x+w-1;y+h-1;1];
+    xy1 = Wout*[tracker(1)+tracker(3)/2;tracker(2)+tracker(4)/2;1];
 %    x1 = xy1
-    new_tracker = [xy1(1), xy1(2), w, h];
+    new_tracker = [xy1(1)/xy1(3)-w/2, xy1(2)/xy1(3)-h/2, w, h];
 
     clf;
     hold on;
-    imshow(im);   
+%    s = subplot(1,2,1)
+    imshow(im);
+    title(sprintf('%i/%i',i,r(end)))
     rectangle('Position', new_tracker, 'EdgeColor', [1 1 0]);
     drawnow;
+    F = getframe(gca);
+    writeVideo(mov, F.cdata);
 
     prev_frame = im;
     tracker = new_tracker;
@@ -53,3 +80,4 @@ for i = 21:280
 %    pause;
 end
 
+close(mov);
