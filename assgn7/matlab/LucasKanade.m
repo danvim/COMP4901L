64889278f45b226fb02eb8ed64506eb90d7c90f1
CHEUNG <dcheungaa@ust.hk>
2019-12-02 16:32:11 +0800

    Exported LK video

--- assgn7/matlab/LucasKanade.m
+++ assgn7/matlab/LucasKanade.m
@@ -1,16 +1,20 @@
 function [u,v] = LucasKanade(It, It1, rect)
 
-th = 0.01;
+th = 1e-6;
 maxIter = 100;
 
-
+It = double(It);
 It1 = double(It1);
 x = rect(1);
 y = rect(2);
 w = rect(3);
 h = rect(4);
 
-it = double(It(y:y+h, x:x+w));
+%[X_, Y_] = meshgrid(1:w+1, 1:h+1);
+%it = interp2(It, X_+x, Y_+y);
+x_ = round(x);
+y_ = round(y);
+it = It(y_:y_+h, x_:x_+w);
 
 s = fspecial('sobel');
 
@@ -20,14 +24,15 @@ iy = imfilter(it, s, 'replicate');
 u = 0;
 v = 0;
 
-[Y, X] = meshgrid(y:y+h, x:x+w);
+[X, Y] = meshgrid(x:x+w, y:y+h);
 
 for i = 1:maxIter
-    it1 = interp2(It1, X+u, Y+v)' - it;
+    it1 = interp2(It1, X+u, Y+v) - it;
+    %it1 = It1(y_+round(v):y_+h+round(v), x_+round(u):x_+w+round(u)) - it;
     vel = [ix(:), iy(:)] \ it1(:);
     u = u + vel(1);
     v = v + vel(2);
-    if max(abs(vel)) < th
+    if u^2 + v^2 < th
         break;
     end
 end
--- assgn7/matlab/lk_demo.m
+++ assgn7/matlab/lk_demo.m
@@ -1,25 +1,35 @@
-tracker = [1 1 100 100]         % TODO Pick a bounding box in the format [x y w h]
+px = 218;
+py = 218;
+ww = 20;
+
+tracker = [px-ww, py-ww, ww*2, ww*2];
+disp(tracker);
 % You can use ginput to get pixel coordinates
 
 %% Initialize the tracker
 figure;
 
-prev_frame = imread('../data/car/frame001.png');
+prev_frame = imread('../data/car/frame0020.jpg');
 
+mov = VideoWriter('../results/car.mp4');
+open(mov);
 %% Start tracking
-new_tracker = tracker;
-for i = 2:145
-    new_frame = imread(sprintf('../data/car/frame%03d.png', i));
+for i = 20:280
+    new_frame = imread(sprintf('../data/car/frame0%03d.jpg', i));
     [u, v] = LucasKanade(prev_frame, new_frame, tracker);
 
     clf;
     hold on;
     imshow(new_frame);   
-    rectangle('Position', new_tracker, 'EdgeColor', [1 1 0]);
+    rectangle('Position', tracker, 'EdgeColor', [1 1 0]);
     drawnow;
+    
+    F = getframe(gca);
+    writeVideo(mov, F.cdata);
 
     prev_frame = new_frame;
     tracker(1) = tracker(1) + u;
     tracker(2) = tracker(2) + v;
 end
+close(mov);
 
